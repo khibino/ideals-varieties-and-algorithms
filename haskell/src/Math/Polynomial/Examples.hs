c025f681439319f48869def0835b3e5ec44fac04
Kei Hibino <ex8k.hibino@gmail.com>
2014-06-29 00:48:51 +0900

    Add convenient import to execute examples interactively.

--- haskell/src/Math/Polynomial/Examples.hs
+++ haskell/src/Math/Polynomial/Examples.hs
@@ -2,7 +2,9 @@
 module Math.Polynomial.Examples
        ( module Math.Polynomial.Examples.Small
        , module Math.Polynomial.Examples.Chapter2
+       , module Math.Polynomial.Imports
        ) where
 
 import Math.Polynomial.Examples.Small
 import Math.Polynomial.Examples.Chapter2
+import Math.Polynomial.Imports

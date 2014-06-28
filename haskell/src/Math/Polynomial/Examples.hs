e4557aadeb5ab56c20690dce46dfd4ece6005292
Kei Hibino <ex8k.hibino@gmail.com>
2014-06-28 12:47:20 +0900

    Update examples.

--- haskell/src/Math/Polynomial/Examples.hs
+++ haskell/src/Math/Polynomial/Examples.hs
@@ -67,17 +67,23 @@ e0Q :: [(GPolynomial Rational 1, GPolynomial Rational 1)]
 eC2S3_1 :: DegreeOrder o => Polynomial o Rational 3
 eC2S3_1 =  "x"^7 * "y"^2 + "x"^3 * "y"^2 - "y" + 1
 
+eC2S3_1_gr :: Polynomial GrLex Rational 3
+eC2S3_1_gr =  eC2S3_1
+
+eC2S3_1_lex :: Polynomial Lex Rational 3
+eC2S3_1_lex =  eC2S3_1
+
 eC2S3_1a_F :: DegreeOrder o => [Polynomial o Rational 3]
 eC2S3_1a_F =  ["x"*"y"^2 - "x", "x" - "y"^3]
 
-eC2S3_1a_qr :: DegreeOrder o => (PolyQuots o Rational 3, Polynomial o Rational 3)
-eC2S3_1a_qr =  eC2S3_1 /. eC2S3_1a_F
+-- eC2S3_1a_qr :: DegreeOrder o => (PolyQuots o Rational 3, Polynomial o Rational 3)
+-- eC2S3_1a_qr =  eC2S3_1 /. eC2S3_1a_F
 
 eC2S3_1a_qr_gr :: (PolyQuots GrLex Rational 3, Polynomial GrLex Rational 3)
-eC2S3_1a_qr_gr =  eC2S3_1a_qr
+eC2S3_1a_qr_gr =  eC2S3_1_gr /. eC2S3_1a_F
 
 eC2S3_1a_qr_lex :: (PolyQuots Lex Rational 3, Polynomial Lex Rational 3)
-eC2S3_1a_qr_lex =  eC2S3_1a_qr
+eC2S3_1a_qr_lex =  eC2S3_1_lex /. eC2S3_1a_F
 
 ppr_eC2S3_1a_qr_gr :: Pretty
 ppr_eC2S3_1a_qr_gr =  pprQuotsRem eC2S3_1a_qr_gr
@@ -85,14 +91,30 @@ ppr_eC2S3_1a_qr_gr =  pprQuotsRem eC2S3_1a_qr_gr
 ppr_eC2S3_1a_qr_lex :: Pretty
 ppr_eC2S3_1a_qr_lex = pprQuotsRem eC2S3_1a_qr_lex
 
-eC2S3_1_gr :: Polynomial GrLex Rational 3
-eC2S3_1_gr =  eC2S3_1
-
-eC2S3_1_lex :: Polynomial Lex Rational 3
-eC2S3_1_lex =  eC2S3_1
-
 checkC2S3_1a_gr :: Bool
 checkC2S3_1a_gr =  checkQuotsRem eC2S3_1_gr eC2S3_1a_F
 
 checkC2S3_1a_lex :: Bool
 checkC2S3_1a_lex = checkQuotsRem eC2S3_1_lex eC2S3_1a_F
+
+
+eC2S3_1b_F :: DegreeOrder o => [Polynomial o Rational 3]
+eC2S3_1b_F =  ["x" - "y"^3, "x"*"y"^2 - "x"]
+
+eC2S3_1b_qr_gr :: (PolyQuots GrLex Rational 3, Polynomial GrLex Rational 3)
+eC2S3_1b_qr_gr =  eC2S3_1_gr /. eC2S3_1b_F
+
+eC2S3_1b_qr_lex :: (PolyQuots Lex Rational 3, Polynomial Lex Rational 3)
+eC2S3_1b_qr_lex =  eC2S3_1_lex /. eC2S3_1b_F
+
+ppr_eC2S3_1b_qr_gr :: Pretty
+ppr_eC2S3_1b_qr_gr =  pprQuotsRem eC2S3_1b_qr_gr
+
+ppr_eC2S3_1b_qr_lex :: Pretty
+ppr_eC2S3_1b_qr_lex = pprQuotsRem eC2S3_1b_qr_lex
+
+checkC2S3_1b_gr :: Bool
+checkC2S3_1b_gr =  checkQuotsRem eC2S3_1_gr eC2S3_1b_F
+
+checkC2S3_1b_lex :: Bool
+checkC2S3_1b_lex = checkQuotsRem eC2S3_1_lex eC2S3_1b_F
